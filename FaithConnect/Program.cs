3dc16666be41edbbea18d362be3d0371c284e76f
Philip El Davids <olorunfemiayanfe@gmail.com>
2026-06-09 18:52:56 +0100

    added cors policy

--- FaithConnect/Program.cs
+++ FaithConnect/Program.cs
@@ -123,7 +123,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", p => p
     //WithOrigins("https://www.zucohr.com")
-    .WithOrigins("http://localhost:5173")
+    //.WithOrigins("http://localhost:5173")
+    .WithOrigins("https://faithconnect8.vercel.app/")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
